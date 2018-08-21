53cf89eb1a2cea5edd66abaf9506a0d061d8dfed
doutor0303 <csh0303@gmail.com>
2018-08-22 01:34:50 +0900

    - add font color of listbox.  - modify main layout.  - insert registration token dialog for Telegram.  - change method from instance to static method.

--- LineageMObserver/Utils.cpp
+++ LineageMObserver/Utils.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include "Utils.h"
 
-
 Utils::Utils()
 {
 }
@@ -36,6 +35,119 @@ bool Utils::executeShell(const wchar_t *file, const wchar_t *parm)
 		return true;
 	}
 	return false;
+}
 
-	return false;
+
+bool Utils::hBitmapToMat(HBITMAP hBmp, Mat &img)
+{
+	// TODO: 여기에 구현 코드 추가.
+	BITMAP bmp;
+
+	GetObject(hBmp, sizeof(BITMAP), &bmp);
+
+	int nChannels = bmp.bmBitsPixel == 1 ? 1 : bmp.bmBitsPixel / 8;
+
+	int depth = bmp.bmBitsPixel == 1 ? IPL_DEPTH_1U : IPL_DEPTH_8U;
+
+	Mat imgT(bmp.bmHeight, bmp.bmWidth, CV_MAKETYPE(depth, nChannels));
+
+	memcpy(imgT.data, (char*)(bmp.bmBits), bmp.bmHeight*bmp.bmWidth*nChannels);
+
+	imshow("name", imgT);
+
+//	img = imgT;
+
+	return true;
+}
+
+
+int Utils::SavefileFromBitmap(HBITMAP hBitmap, LPCTSTR lpszFileName)
+{
+	// TODO: 여기에 구현 코드 추가.
+	HDC hDC;
+	int iBits;
+	WORD wBitCount;
+	DWORD dwPaletteSize = 0, dwBmBitsSize = 0, dwDIBSize = 0, dwWritten = 0;
+	BITMAP Bitmap;
+	BITMAPFILEHEADER bmfHdr;
+	BITMAPINFOHEADER bi;
+	LPBITMAPINFOHEADER lpbi;
+	HANDLE fh, hDib, hPal, hOldPal = NULL;
+
+	hDC = ::CreateDC(_T("DISPLAY"), NULL, NULL, NULL);
+	iBits = ::GetDeviceCaps(hDC, BITSPIXEL) * ::GetDeviceCaps(hDC, PLANES);
+	::DeleteDC(hDC);
+	if (iBits <= 1)
+		wBitCount = 1;
+	else if (iBits <= 4)
+		wBitCount = 4;
+	else if (iBits <= 8)
+		wBitCount = 8;
+	else
+		wBitCount = 24;
+	GetObject(hBitmap, sizeof(Bitmap), (LPSTR)&Bitmap);
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = Bitmap.bmWidth;
+	bi.biHeight = Bitmap.bmHeight;
+	bi.biPlanes = 1;
+	bi.biBitCount = wBitCount;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrImportant = 0;
+	bi.biClrUsed = 0;
+	dwBmBitsSize = ((Bitmap.bmWidth * wBitCount + 31) / 32) * 4 * Bitmap.bmHeight;
+
+	hDib = GlobalAlloc(GHND, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
+	lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);
+	*lpbi = bi;
+
+	hPal = GetStockObject(DEFAULT_PALETTE);
+	if (hPal)
+	{
+		hDC = ::GetDC(NULL);
+		hOldPal = ::SelectPalette(hDC, (HPALETTE)hPal, FALSE);
+		::RealizePalette(hDC);
+	}
+
+
+	GetDIBits(hDC, hBitmap, 0, (UINT)Bitmap.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER)
+		+ dwPaletteSize, (BITMAPINFO *)lpbi, DIB_RGB_COLORS);
+
+	if (hOldPal)
+	{
+		::SelectPalette(hDC, (HPALETTE)hOldPal, TRUE);
+		::RealizePalette(hDC);
+		::ReleaseDC(NULL, hDC);
+	}
+
+	fh = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+
+	if (fh == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	bmfHdr.bfType = 0x4D42; // "BM"
+	dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize;
+	bmfHdr.bfSize = dwDIBSize;
+	bmfHdr.bfReserved1 = 0;
+	bmfHdr.bfReserved2 = 0;
+	bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize;
+
+	WriteFile(fh, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+
+	WriteFile(fh, (LPSTR)lpbi, dwDIBSize, &dwWritten, NULL);
+	GlobalUnlock(hDib);
+	GlobalFree(hDib);
+	CloseHandle(fh);
+
+	return 0;
+}
+
+
+int Utils::SavePNGFromBitmapUsingOpenCV(HBITMAP bitmap)
+{
+	// TODO: 여기에 구현 코드 추가.
+	return 0;
 }
--- LineageMObserver/Utils.h
+++ LineageMObserver/Utils.h
@@ -1,9 +1,20 @@
 #pragma once
+
+#include <opencv2\core\core.hpp>
+#include <opencv2\opencv.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+using namespace cv;
+
 class Utils
 {
 public:
 	Utils();
 	~Utils();
-	bool executeShell(const wchar_t *file, const wchar_t *parm);
+	static bool executeShell(const wchar_t *file, const wchar_t *parm);
+	static bool hBitmapToMat(HBITMAP hBmp, Mat &img);
+	int SavefileFromBitmap(HBITMAP hBitmap, LPCTSTR lpszFileName);
+	int SavePNGFromBitmapUsingOpenCV(HBITMAP bitmap);
 };
 
